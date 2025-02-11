b5d0d0dd2e928df72125728f8a070f84bc09fd8a
seoeongeueun <erin9901@gmail.com>
2025-02-11 19:22:29 +0900

    chore: metadata 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,26 @@
 import type {Metadata} from "next";
 import "./globals.css";
 
-export const metadata: Metadata = {title: "workmate", description: "work with me!", viewport: "width=device-width, initial-scale=1"};
+export const metadata: Metadata = {
+	title: "workmate",
+	description: "Work with me! 📻",
+	icons: "/icon/alien.ico",
+	openGraph: {
+		title: "workmate",
+		description: "A retro music player true old-school lovers 📻",
+		url: "https://workmatebeta.vercel.app",
+		siteName: "Workmate",
+		images: [
+			{
+				url: "/main.png",
+				width: 1200,
+				height: 630,
+				alt: "OpenGraph Preview",
+			},
+		],
+		type: "website",
+	},
+};
 
 export default function RootLayout({children}: Readonly<{children: React.ReactNode}>) {
 	return (
