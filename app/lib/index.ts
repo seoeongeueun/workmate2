ca130f6504dfb59fbe296fc036c1da01ecdcd369
seoeongeueun <erin9901@gmail.com>
2026-03-08 07:12:03 +0900

    refactor: update dbConnect import paths and streamline type exports across multiple files

--- app/lib/index.ts
+++ app/lib/index.ts
@@ -1,6 +1,5 @@
 export {apiRequest, ApiError} from "./apiRequest";
 export {extractVideoId} from "./tools";
-export {dbConnect} from "./dbConnect";
 export {MAX_AGE} from "./constants";
-export {MONGODB_URI, QUERY_CLIENT_DEFAULT_OPTIONS} from "./api";
+export {QUERY_CLIENT_DEFAULT_OPTIONS} from "./api";
 export {sessionOptions} from "./session";
