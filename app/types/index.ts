db6662f1474c6d8ccd73a155e81920ec81ede313
seoeongeueun <erin9901@gmail.com>
2026-03-08 11:37:14 +0900

    refactor: implement button store and define button types

--- app/types/index.ts
+++ app/types/index.ts
@@ -1,3 +1,4 @@
 export type {Track, Playlist, PlaylistInfo} from "./playlist";
 export type {SessionData, SessionInfo, SessionExpirationResponse} from "./session";
 export type {ApiResponse, ApiSuccess, ApiError} from "./response";
+export type {ButtonValue} from "./button";
