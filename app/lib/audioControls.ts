3309e34e3e26e974efd1e4077e0896286ca20770
seoeongeueun <erin9901@gmail.com>
2026-03-08 13:04:59 +0900

    refactor: clean up audioControls and export functions directly

--- app/lib/audioControls.ts
+++ app/lib/audioControls.ts
@@ -3,28 +3,21 @@ const Power = "/music/power.mp3";
 type AudioKey = "power";
 
 const audios: Record<AudioKey, HTMLAudioElement> = {
-    power: typeof window !== "undefined" ? new Audio(Power) : null as unknown as HTMLAudioElement,
+	power: typeof window !== "undefined" ? new Audio(Power) : (null as unknown as HTMLAudioElement),
 };
 
 let current: AudioKey | null = null;
 
-const play = (name: AudioKey): void => {
-    if (current) {
-        audios[current].pause();
-    }
+export const play = (name: AudioKey): void => {
+	if (current) {
+		audios[current].pause();
+	}
 
-    audios[name].play();
-    current = name;
-}
-
-const pause = (name: AudioKey): void => {
-    current = null;
-    audios[name].pause();
-}
-
-const audioControls = {
-  pause,
-  play,
+	audios[name].play();
+	current = name;
 };
 
-export default audioControls;
+export const pause = (name: AudioKey): void => {
+	current = null;
+	audios[name].pause();
+};
--- app/lib/index.ts
+++ app/lib/index.ts
@@ -3,3 +3,4 @@ export {extractVideoId} from "./tools";
 export {MAX_AGE} from "./constants";
 export {QUERY_CLIENT_DEFAULT_OPTIONS} from "./api";
 export {sessionOptions} from "./session";
+export {play, pause} from "./audioControls";
