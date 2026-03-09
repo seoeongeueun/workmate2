e11bb9425ac3467abbe852c946eba5819c001706
seoeongeueun <erin9901@gmail.com>
2026-03-10 02:42:14 +0900

    refactor: implement DialogueScreen component and integrate dialogue choices

--- app/lib/index.ts
+++ app/lib/index.ts
@@ -1,6 +1,6 @@
 export {apiRequest, ApiError} from "./apiRequest";
 export {extractVideoId} from "./tools";
-export {MAX_AGE} from "./constants";
+export {MAX_AGE, DIALOGUE_CHOICES} from "./constants";
 export {QUERY_CLIENT_DEFAULT_OPTIONS} from "./api";
 export {sessionOptions} from "./session";
 export {play, pause} from "./audioControls";
